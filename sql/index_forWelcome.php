f8bb8616b079f91d900e779a1a46fc6110fc6025
EFB57 <93217818+EFB57@users.noreply.github.com>
2022-11-01 00:45:59 -0400

    Create index_forWelcome.php
    Template, mostly

---
+++ sql/index_forWelcome.php
@@ -0,0 +1,14 @@
+<html>
+<body>
+
+<form action = "welcome.php" method = "post">
+Student ID: <input type = "number" name = "id"><br>
+Name: <input type = "text" name = "name"><br>
+Preferred Room Type: <input type = "text" name = "roomType"><br>
+//Could add more stuff here, maybe even make preferred room type a dropdown 
+<input type = "submit">
+
+</form>
+
+</body>
+</html>
